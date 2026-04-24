d33d5f649f1c52243d31573dfecbff0e3f247263
Christian A. Dampil <chanskiadolfo@gmail.com>
2026-04-24 09:41:44 +0800

    Midterm Lab Submission

---
+++ student.js
@@ -0,0 +1,14 @@
+
+var studentName   = "Dampil, Christian";   
+var studentCourse = "BSIT";             
+var studentYear   = "3rd Year";          
+var favLang       = "JavaScript";        
+
+console.log("============================");
+console.log("    STUDENT INFORMATION     ");
+console.log("============================");
+console.log("Name    : " + studentName);
+console.log("Course  : " + studentCourse);
+console.log("Year    : " + studentYear);
+console.log("Fav Lang: " + favLang);
+console.log("============================");
